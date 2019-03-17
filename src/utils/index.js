ba85d0532a31c7e6195076181f3d89b48f67043c
iKurisu <chris.epic01@gmail.com>
2019-03-17 19:33:51 +0100

    Add utilities to manipulate local storage data

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+export const getLists = () => {
+  return JSON.parse(localStorage.getItem('lists') || '[]]');
+}
+
+export const post = list => {
+  const lists = getLists();
+
+  lists.push(list);
+
+  localStorage.setItem('lists', JSON.stringify(lists));
+}
