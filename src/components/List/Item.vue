1140d26e62ea46c8053dc9c19af0d1e78c304a21
iKurisu <chris.epic01@gmail.com>
2019-03-17 11:52:47 +0100

    Add file created before repository

---
+++ src/components/List/Item.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="list-item" 
+    @click="[setActive(item.id), setTodo(item.title)]"
+    @mouseover="setHover(true)" 
+    @mouseleave="setHover(false)"
+    :style="{ transform: `translate3d(0, ${offset}px, 0)` }"
+  >
+    <p>{{ item.title.toUpperCase() }}</p>
+    <HoverLine :hovering="hovering" :isActive="isActive" />
+  </div>
+</template>
+
+<script>
+import HoverLine from '../VHoverLine';
+
+export default {
+  name: 'ListItem',
+  components: {
+    HoverLine
+  },
+  props: {
+    item: Object,
+    offset: Number,
+    isActive: Boolean,
+    setActive: Function,
+    setTodo: Function
+  },
+  data() {
+    return {
+      hovering: false
+    }
+  },
+  methods: {
+    setHover(bool) {
+      this.hovering = bool;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.list-item {
+  margin-bottom: 16px;
+  cursor: pointer;
+  will-change: transform;
+  transition: transform .5s ease;
+}
+
+.list-item p {
+  color: #1a1a1a;
+  width: 100%;
+}
+</style>
