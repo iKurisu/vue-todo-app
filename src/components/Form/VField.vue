45268edc5673b4e1f27789e9ffc330a45fd7d513
iKurisu <chris.epic01@gmail.com>
2019-03-17 12:00:14 +0100

    Add partial form

---
+++ src/components/Form/VField.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    :class="[
+      'form-field', 
+      length > 0 ? 'form-field--visible' : 'form-field--hidden'
+    ]"
+  >
+    <p>{{ name }}</p>
+    <input 
+      type="text" 
+      spellcheck="false"
+      :value="value" 
+      @input="$emit('input', $event.target.value)"
+      @focus="setFocus(true)" @blur="setFocus(false)"
+    >
+    <div :class="['form-field-line', { 'form-field-line--active': focused }]" />
+    <label>{{ name }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormField",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    length: {
+      type: Number,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  }, 
+  data() {
+    return {
+      focused: false
+    }
+  },
+  methods: {
+    setFocus(bool) {
+      this.focused = bool;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.form-field {
+  width: 15%;
+  margin-bottom: 20px;
+}
+
+.form-field p {
+  font-size: 12px;
+  transition: all 0.3s ease;
+}
+
+.form-field label {
+  opacity: 0.6;
+  position: relative;
+  top: -28px;
+}
+
+.form-field--hidden p {
+  transform: translateY(12px);
+  opacity: 0;
+}
+
+.form-field--visible p {
+  transform: translateY(0);
+  opacity: 1;
+}
+
+.form-field--visible label {
+  opacity: 0;
+}
+
+.form-field--hidden label {
+  transition: opacity 0.5s ease;
+}
+
+.form-field-line {
+  width: 0;
+  border-bottom: 1px solid #555;
+  position: relative;
+  top: -1px;
+  z-index: 3;
+  transition: width .3s ease;
+}
+
+.form-field-line--active {
+  width: 100%;
+}
+</style>
+
---
+++ src/components/Form/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="form">
+    <FormField 
+      :name="'TITLE'" 
+      :length="title.length" 
+      :value="title" 
+      @input="updateTitle" 
+    /> 
+    <FormField 
+      :name="'ADD TODO'" 
+      :length="todo.length" 
+      :value="todo" 
+      @input="updateTodo" 
+    />
+  </div>
+</template>
+
+<script>
+import FormField from './VField';
+
+export default {
+  name: "Form",
+  components: {
+    FormField
+  },
+  data() {
+    return {
+      title: '',
+      todo: ''
+    }
+  },
+  methods: {
+    updateTitle(value) {
+      this.title = value.toUpperCase();
+    },
+    updateTodo(value) {
+      this.todo = value.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 20vh;
+}
+
+.form p {
+  margin-bottom: 6px;
+  color: #333;
+}
+
+.form input {
+  background: transparent;
+  width: 100%;
+  height: 32px;
+  padding-bottom: 2px;
+  border-bottom: 1px solid #d8d7d6;
+
+  position: relative;
+  z-index: 2;
+}
+</style>
+
+
