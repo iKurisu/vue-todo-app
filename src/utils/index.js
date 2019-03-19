01d046f735fcfd5a60278ec3d4684e0f5ad53a5f
iKurisu <chris.epic01@gmail.com>
2019-03-19 19:24:34 +0100

    Add update list utility

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,16 @@ export const updateLists = lists => {
   localStorage.setItem('lists', JSON.stringify(lists));
 }
 
+export const updateList = (id, editedList) => {
+  const lists = getLists();
+
+  for (let property in editedList) {
+    lists[id][property] = editedList[property];
+  }
+
+  updateLists(lists);
+}
+
 export const updateTodo = (listId, todoId) => {
   const lists = getLists();
   const checked = lists[listId].todos[todoId].checked;
