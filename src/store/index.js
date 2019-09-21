90cc2d23fab6c62ef5be48b1eedb68ab72fa10c1
iKurisu <chris.epic01@gmail.com>
2019-09-21 08:20:26 +0200

    Add todoLists module

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import view from "./modules/view";
+import todoLists from "./modules/todoLists";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
-    view
+    view,
+    todoLists
   }
 });
 
