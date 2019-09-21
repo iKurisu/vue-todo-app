fc330a61cb8f4ee4cb60deeac19054f9ef38faf3
iKurisu <chris.epic01@gmail.com>
2019-09-21 08:17:29 +0200

    Move todo lists logic to vuex

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <template>
   <div id="app">
-    <Navigation :delete-list="deleteList" :set-form-type="setFormType" />
-    <MainView
-      v-if="list.length > 0"
-      :list="list"
-      :active-id="activeId"
-      :set-active="setActive"
-      :check-todo="checkTodo"
-    />
-    <FormView
-      :active-list="list[activeId]"
-      :active-id="activeId"
-      :type="formType"
-      :add-list="addList"
-      :update="update"
-    />
+    <Navigation :set-form-type="setFormType" />
+    <MainView />
+    <FormView :type="formType" />
   </div>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import { mapMutations, mapActions } from "vuex";
 import MainView from "./views/Main";
 import FormView from "./views/Form";
 import Navigation from "./components/Navigation";
-import { getLists, updateTodo, updateLists, updateList } from "./utils";
 
 export default {
   name: "App",
@@ -34,61 +21,18 @@ export default {
   },
   data() {
     return {
-      list: [],
-      activeId: 0,
+      activeId: "",
       formType: "new"
     };
   },
   created() {
-    this.list = getLists();
-    if (this.list.length > 0) {
-      this.list[0].isActive = true;
-      this.activeId = 0;
-      this.changeView("Main");
-    } else {
-      this.changeView("Form");
-    }
+    this.loadLists();
   },
   methods: {
     ...mapMutations(["changeView"]),
+    ...mapActions(["loadLists"]),
     setFormType(type) {
       this.formType = type;
-    },
-    addList(list) {
-      this.list.push(list);
-      this.setActive(this.list.length - 1);
-    },
-    update(list) {
-      const { name, dueDate, todos } = list;
-
-      updateList(this.activeId, { name, dueDate, todos });
-
-      for (let property in list) {
-        this.list[this.activeId][property] = list[property];
-      }
-    },
-    deleteList() {
-      this.list = this.list.filter((_, i) => i !== this.activeId);
-
-      if (this.list.length) {
-        this.setActive(0);
-      }
-
-      updateLists(this.list);
-    },
-    setActive(id) {
-      if (this.list[this.activeId]) {
-        this.list[this.activeId].isActive = false;
-      }
-
-      this.list[id].isActive = true;
-      this.activeId = id;
-    },
-    checkTodo(id) {
-      const { todos } = this.list[this.activeId];
-
-      this.list[this.activeId].todos[id].checked = !todos[id].checked;
-      updateTodo(this.activeId, id);
     }
   }
 };
---
+++ src/store/modules/todoLists.js
@@ -0,0 +1,87 @@
+import { getLists, updateLists as updateStoredLists } from "../../utils";
+
+const state = {
+  lists: [],
+  activeListId: ""
+};
+
+const getters = {
+  activeList({ lists, activeListId }) {
+    return (
+      lists.find(list => list.id === activeListId) || {
+        title: "",
+        dueDate: "",
+        todos: [],
+        id: ""
+      }
+    );
+  }
+};
+
+const mutations = {
+  addStoredLists(state) {
+    state.lists = getLists();
+  },
+  pushList(state, list) {
+    state.lists.push(list);
+  },
+  setActiveListId(state, id) {
+    state.activeListId = id;
+  },
+  updateList(state, updatedList) {
+    state.lists = state.lists.map(list =>
+      list.id === updatedList.id ? updatedList : list
+    );
+  },
+  deleteList(state) {
+    state.lists = state.lists.filter(list => list.id !== state.activeListId);
+  },
+  checkTodo(state, todoId) {
+    state.lists = state.lists.map(list =>
+      list.id === state.activeListId
+        ? {
+            ...list,
+            todos: list.todos.map(todo =>
+              todo.id === todoId ? { ...todo, checked: !todo.checked } : todo
+            )
+          }
+        : list
+    );
+  }
+};
+
+const actions = {
+  loadLists({ state, commit }) {
+    commit("addStoredLists");
+
+    if (state.lists.length > 0) {
+      commit("setActiveListId", state.lists[0].id);
+      commit("changeView", "Main", { root: true });
+    }
+  },
+  addList({ state, commit }, list) {
+    commit("pushList", list);
+    commit("setActiveListId", list.id);
+    updateStoredLists(state.lists);
+  },
+  updateList({ state, commit }, updatedList) {
+    commit("updateList", updatedList);
+    updateStoredLists(state.lists);
+  },
+  deleteList({ state, commit }) {
+    commit("deleteList");
+    commit("setActiveListId", state.lists.length > 0 ? state.lists[0].id : "");
+    updateStoredLists(state.lists);
+  },
+  checkTodo({ state, commit }, todoId) {
+    commit("checkTodo", todoId);
+    updateStoredLists(state.lists);
+  }
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+};
